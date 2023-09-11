de13a427ea395641e595af0f615f8bf1e4773d3e
Max <101028170+MaxCPietro@users.noreply.github.com>
2023-09-11 13:47:00 -0300

    Primer Commit Clase 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Tarea1 from "./tarea/Tarea1";
-import Contador from "./tarea/Contador";
-import Estados from "./temario/Estados";
+//import Tarea1 from "./tarea/Tarea1";
+//import Contador from "./tarea/Contador";
+//import Estados from "./temario/Estados";
+import ReelCartas from "./tareas/ReelCartas";
 
 const App = () => {
   return (
     <>
-      <Contador />
+      <ReelCartas />
     </>
   );
 };
---
+++ src/tareas/ReelCartas.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react"
+
+const ReelCartas = () => {
+
+    return (
+        <div>
+            <button onClick={() => {}}>{"->"}</button>
+            <p>{"Nombre"}</p>
+            <p>{"Edad"}</p>
+            <button onClick={() => {}}>{"<-"}</button>
+        </div>
+    )
+}
+
+export default ReelCartas
