4e476f8332f0827ec0a8a0c58b257b86b77429bd
Max <101028170+MaxCPietro@users.noreply.github.com>
2023-09-11 21:36:53 -0300

    Segunda parte clase 10

--- src/tareas/ReelCartas.jsx
+++ src/tareas/ReelCartas.jsx
@@ -1,15 +1,20 @@
 import React, {useState} from "react"
 
-const ReelCartas = () => {
+const ReelCartas = ({users}) => {
+
+    const [index,setIndex] = useState(0);
+
+    const handleNextUser = () => {
+        setIndex(index + 1);
+    };
 
     return (
         <div>
-            <button onClick={() => {}}>{"->"}</button>
-            <p>{"Nombre"}</p>
-            <p>{"Edad"}</p>
+            <button onClick={handleNextUser}>{"->"}</button>
+            <p>{JSON.stringify(users[index])}</p>
             <button onClick={() => {}}>{"<-"}</button>
         </div>
-    )
+    );
 }
 
 export default ReelCartas
