e65e5141ad4c18570b76d7acf6014b9fb037f9ca
Max <101028170+MaxCPietro@users.noreply.github.com>
2023-10-04 18:27:01 -0300

    se agrego la parte de error de la API

--- src/tareas/Api_Gatitos.jsx
+++ src/tareas/Api_Gatitos.jsx
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react'
 
 const Api_Gatitos = () => {
-
+    //Estados de la API REQUEST: LOADED, LOADING y REJECTED
     const [fotoGato,setFotoGato] = useState('');
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
     
     const handleOtroGato = () => {traerGatoApi()}
 
     const traerGatoApi = async () => {  
-        const endpoint = 'https://api.thecatapi.com/v1/images/search';
-        const response = await fetch (endpoint);
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
-        const gatitoPic = data[0].url;
-        setFotoGato(gatitoPic);
+        setError(null);
+        setIsLoading(true);
+        try {
+            const endpoint = 'https://api.thecatapi.com/v1/images/search';
+            const response = await fetch (endpoint);
+            
+            //capturo si no hay rta de la API
+            if(!response.ok) {
+                throw new Error('ESTA API NO FUNCIONA');
+            }
+            console.log(response);
+            const data = await response.json();
+            console.log(data);
+            const gatitoPic = data[0].url;
+            setFotoGato(gatitoPic);
+        } 
+        catch (e) {setError(e.message);} 
+        finally {setIsLoading(false);} 
     }
 
     useEffect (() => {
@@ -24,10 +37,19 @@ const Api_Gatitos = () => {
         <div>
             <div><h1>Api Gatitos</h1></div>
             <div><button onClick={handleOtroGato}>Otro Gato</button></div>
-            <div><img src={fotoGato}
+            <div>
+                {isLoading && <h1>CARGANDO!!</h1>}
+
+                {error && <h1>{error}</h1>}
+
+                {
+                <img src={fotoGato}
                 alt="gato de la API thecatapi.com" 
                 title="gatito_radom"
-                width="642" height="639" /></div>
+                width="642" height="639" />
+                }
+
+            </div>
         </div>
     )
 }
